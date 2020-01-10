573190bb3adbae048a70bc11c2ba44786c344cce
lawrik <lawr_fenix@mail.ru>
2020-01-10 16:18:04 +0300

    Добавление конструктора

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -9,6 +9,13 @@ namespace Snake
         public int x;
         public int y;
         public int sym;
+        public Point() { }
+
+        public Point(int x,int y, char sym) {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+        }
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
