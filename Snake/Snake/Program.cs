1757e9aaaffce042a62b06f921ada0cce6596dca
lawrik <lawr_fenix@mail.ru>
2020-01-10 15:15:20 +0300

    Добавили функцию рисования

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -6,7 +6,18 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HelloWorld");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+            Draw(x1, y1, sym1);
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
