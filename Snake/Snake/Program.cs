4c56b19f772d725d68ff079f574fbbb7c79c6ee1
lawrik <lawr_fenix@mail.ru>
2020-01-22 18:09:56 +0300

    Add List

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Snake
 {
@@ -7,73 +8,37 @@ namespace Snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            //p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            //p2.Draw();
 
+            List<int> list = new List<int>();
+            list.Add(p1.x);
+            list.Add(1);
+            list.Add(2);
 
-            /*Console.WriteLine($"p1 = {p1.x}, {p1.y}, {p1.sym}");
-            Console.WriteLine($"p2 = {p2.x}, {p2.y}, {p2.sym}");
+            int x = list[0];
+            int y = list[1];
+            int z = list[2];
 
-            p1 = p2;
-            Console.WriteLine($"p1 = p2");
+            foreach(int i in list)
+            {
+                Console.WriteLine(i);
+            }
 
-            Console.WriteLine($"p1 = {p1.x}, {p1.y}, {p1.sym}");
-            Console.WriteLine($"p2 = {p2.x}, {p2.y}, {p2.sym}");
+            list.RemoveAt(0);
 
-            p2.x++;
-            p2.y++;
-            Console.WriteLine($"p2++");
-            Console.WriteLine($"p1 = {p1.x}, {p1.y}, {p1.sym}");
-            Console.WriteLine($"p2 = {p2.x}, {p2.y}, {p2.sym}");*/
+            List<Point> pList = new List<Point>();
 
+            Console.WriteLine($"p1.x = {p1.x}, x = {x}, list(0)={list[0]}");
+            Console.WriteLine($"y = {y}, list(1)={list[1]}");
 
-            int x = 1;
-            Func1(x);
-            Console.WriteLine($"Func1 x={x}");
-
-            Func2(x);
-            Console.WriteLine($"Func2 x={x}");
-
-            Func3(x);
-            Console.WriteLine($"Func3 x={x}");
-
-            Move(p1, 10, 10);
-            Console.WriteLine($"Move(p1, 10, 10) p1.x = {p1.x}, p1.y = {p1.y}");
-
-            Reset(p2);
-            Console.WriteLine($"Reset(p2) p2.x = {p2.x}, p2.y = {p2.y}");
+            p1.x++;
+            Console.WriteLine($"p1.x = {p1.x}, x = {x}, list(0)={list[0]}");
+            Console.WriteLine($"y = {y}, list(1)={list[1]}");
 
             Console.ReadKey();
-        }
-
-
-        private static void Reset(Point p)
-        {
-            p = new Point();
-            //  Здесь переменная p2 останется без изменений.
-        }
-
-        private static void Move(Point p, int dx, int dy)
-        {
-            p.x = p.x + dx;
-            p.y = p.y + dy;
-        }
-
-        private static void Func3(int x)
-        {
-            x++;
-        }
-
-        private static void Func2(int val)
-        {
-            val++;
-        }
-
-        private static void Func1(int x)
-        {
-
         }
     }
 }
