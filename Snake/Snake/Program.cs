b3af8b8e6a2967262674e8b1ef584921fcd2d722
lawrik <lawr_fenix@mail.ru>
2020-01-29 15:55:25 +0300

    Добавил сообщение "Игра готова"

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,45 +10,62 @@ namespace Snake
         {
             Console.SetBufferSize(120,30);
 
-            HorisontalLine upLine = new HorisontalLine(0,78,0,'#');
-            VerticalLine leftLine = new VerticalLine(0,0,23,'#');
-            HorisontalLine downLine = new HorisontalLine(0,78,23,'#');
-            VerticalLine rightLine = new VerticalLine(78,0,23,'#');
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
 
-            upLine.Drow();
-            leftLine.Drow();
-            downLine.Drow();
-            rightLine.Drow();
+            // Отрисовка точек			
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
 
-            Point p = new Point(3,4,'*');
-            Snake snake = new Snake(p,4,Direction.RIGHT);
-            snake.Drow();
-
-            FoodCreator createFood = new FoodCreator(80,25,'$');
-            Point food = createFood.CreateFood();
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
             food.Draw();
 
             while (true)
             {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
                 if (snake.Eat(food))
                 {
-                    food = createFood.CreateFood();
+                    food = foodCreator.CreateFood();
                     food.Draw();
                 }
                 else
-                { snake.Move(); }
-                
+                {
+                    snake.Move();
+                }
+
                 Thread.Sleep(100);
-                
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandlKey(key.Key);
                 }
             }
-            
 
+            WriteGameOver();
             Console.ReadKey();
+        }
+
+        static void WriteGameOver()
+        {
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+            WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
+            yOffset++;
+            WriteText("============================", xOffset, yOffset++);
+        }
+
+        static void WriteText(String text, int xOffset, int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
         }
     }
 }
---
+++ Snake/Snake/Walls.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Snake
+{
+    internal class Walls
+    {
+        List<Figure> wallList;
+        public Walls(int wight, int height) {
+            wallList = new List<Figure>();
+
+            HorisontalLine upLine = new HorisontalLine(0, wight - 2, 0, '#');
+            VerticalLine leftLine = new VerticalLine(0, 0, height-2, '#');
+            HorisontalLine downLine = new HorisontalLine(0, wight-2, height-2, '#');
+            VerticalLine rightLine = new VerticalLine(wight-2, 0, height-2, '#');
+
+            wallList.Add(upLine);
+            wallList.Add(leftLine);
+            wallList.Add(downLine);
+            wallList.Add(rightLine);
+        }
+
+        internal bool IsHit(Figure f)
+        {
+            foreach (var wall in wallList)
+            {
+                if (wall.IsHit(f)) { return true; } 
+            }
+            return false;
+        }
+
+        public void Draw() {
+            foreach (var wall in wallList) { 
+                wall.Draw();
+            }
+        }
+    }
+}
